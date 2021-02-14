06b424e7d9cee501aee22611be72e4f5b95626c2
F-Sukhy <nahar.rnkass@gmail.com>
2021-02-14 15:57:11 +0600

    Animate

---
+++ js/custom.js
@@ -0,0 +1,60 @@
+$('.group #btn').click(function(){
+  
+   $('.box').animate({
+    left: 200,
+    top: 200,
+    borderRadius: "50%",
+    
+},2000);
+
+$('.box').animate({
+    left: 400,
+    top: 400,
+    borderRadius: "50",
+   
+},2000);
+$('.box').animate({
+    left: 400,
+    top: 0,
+    borderRadius: "50%",
+   
+},2000);
+
+$('.box').animate({
+    left: 200,
+    top: 200,
+    borderRadius: "50%",
+},2000);
+$('.box').animate({
+    left: 0,
+    top: 400,
+    borderRadius: "50%",
+},2000);
+$('.box').animate({
+    left: 0,
+    top: 0,
+    borderRadius: "50%",
+},2000);
+$('.box').animate({
+    left: 400,
+    top: 0,
+    borderRadius: "50%",
+},2000);
+$('.box').animate({
+    left: 400,
+    top: 400,
+    borderRadius: "50%",
+},2000);
+$('.box').animate({
+    left: 0,
+    top: 400,
+    borderRadius: "50%",
+},2000);
+$('.box').animate({
+    left: 0,
+    top: 400,
+    borderRadius: "0%",
+},2000);
+
+
+});
